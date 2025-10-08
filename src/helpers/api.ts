04262a3cc90f04a63e3ad29e7421ef3bf02a2263
imanirvansyah <imanirvansyaah@gmail.com>
2025-10-08 11:14:31 +0700

    fix: fix env naming key

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const callIApi = axios.create({
-  baseURL: import.meta.env.REACT_PUBLIC_BASE_URL,
+  baseURL: import.meta.env.VITE_PUBLIC_BASE_URL,
   headers: {
     accept: "application/json",
   }
